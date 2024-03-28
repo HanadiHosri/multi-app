041dc007c2d40de04f550645a005019852de3644
unknown <hanadi.hosri@gmail.com>
2024-03-28 06:41:53 +0200

    adding style.css to weather folder

--- src/pages/weather/index.jsx
+++ src/pages/weather/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "./style.css";
 
 const Weather = () => {
 
     return (
-        <div>
-            <h1>weather</h1>
+        <div className="page">
+            <h1>Beirut</h1>
+            <div className="container">
+                <div className="temp">
+                    <h3>Min temperature 20 °C</h3>
+                    <h3>Max temperature 22 °C</h3>
+                </div>
+                <div className="rain">
+                    <p>Rain sum 0.00 mm</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
