19b1b8f3e936c978158df5af3c0dfb2ce2a474ff
unknown <hanadi.hosri@gmail.com>
2024-03-28 06:10:12 +0200

    creating weather component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./styles/utilities.css";
 
 import Calculator from "./pages/calculator";
+import Weather from "./pages/weather";
 
 function App() {
   return (
     <div className="app">
-      <Calculator/>
+      <Weather/>
     </div>
   );
 }
