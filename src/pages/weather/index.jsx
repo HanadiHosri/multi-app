607c7fdf55706a05fc6b9ab2a5fafa940b45ba6b
unknown <hanadi.hosri@gmail.com>
2024-03-29 06:32:11 +0200

    accessing weather data from api

--- src/pages/weather/index.jsx
+++ src/pages/weather/index.jsx
@@ -4,33 +4,38 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Weather = () => {
-    const [data, setData] = useState({
-        date: "2024-03-27",
-        minTemp: 20,
-        maxTemp: 22,
-        rain: 0.00
-    })
+    const [weatherData, setWeatherData] = useState([]);
 
     useEffect(() => {
         axios.get("https://api.open-meteo.com/v1/forecast?latitude=33.8938&longitude=35.5018&daily=apparent_temperature_max,apparent_temperature_min,rain_sum&forecast_days=4")
-        .then(res => console.log(res))
+        .then(res => {
+            setWeatherData(res.data.daily);
+        })
         .catch(err => console.log(err));
-    }, [])
-    
+    }, []);
+
+    useEffect(() => {
+        console.log(weatherData);
+    }, [weatherData]);
+
     return (
         <div className="page">
             <h1>Beirut Weather</h1>
             <div className="container">
-                <div className="date flex center">
-                    <h2>2024-03-27</h2>
-                </div>
-                <div className="temp flex row">
-                    <h3>Min temperature 20 °C</h3>
-                    <h3>Max temperature 22 °C</h3>
-                </div>
-                <div className="rain">
-                    <p>Rain sum 0.00 mm</p>
-                </div>
+                {weatherData.time.map((day, index) => (
+                    <div key={index} className="weather-container">
+                        <div className="date flex center">
+                            <h2>{day}</h2>
+                        </div>
+                        <div className="temp flex row">
+                            <h3>Min Temperature {weatherData.apparent_temperature_min[index]} °C</h3>
+                            <h3>Max Temperature {weatherData.apparent_temperature_max[index]} °C</h3>
+                        </div>
+                        <div className="rain">
+                            <p>Rain Sum {weatherData.rain_sum[index]} mm</p>
+                        </div>
+                    </div>
+                ))}
             </div>
             
         </div>
